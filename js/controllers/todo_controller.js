6198b0e3f06392548c09ae79d86206ee65d9d9d5
prakritis <prakriti.saxena@imaginea.com>
2016-12-16 10:34:37 +0530

    initial commit

---
+++ js/controllers/todo_controller.js
@@ -0,0 +1,20 @@
+Todos.TodoController = Ember.ObjectController.extend({
+    isCompleted: function(key, value){
+        var model = this.get('model');
+
+        if (value === undefined) {
+            return model.get('isCompleted');
+        } else {
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted'),
+    actions: {
+        editTodo: function() {
+            this.set('isEditing', true);
+        }
+    },
+
+    isEditing: false
+});
